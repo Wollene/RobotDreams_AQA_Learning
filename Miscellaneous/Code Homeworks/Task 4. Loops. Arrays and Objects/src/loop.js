c9684f5306217f1013d350db52387bde577e3519
Wollene <whoifnotwollene@gmail.com>
2026-05-07 21:01:44 +0300

    Added a new folder for Task 4, resolved loop.js task. Changed README.md file in the root folder.

---
+++ Miscellaneous/Code Homeworks/Task 4. Loops. Arrays and Objects/src/loop.js
@@ -0,0 +1,41 @@
+// Iterating from 0 to 9 and from 100 to 0 (with step equals 10) through 'for' loop.
+
+for (let i = 0; i <= 9; i++) {
+    console.log(`[ITERATING THROUGH 'FOR' (0 to 9)] i = ${i}`);
+}
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(`[ITERATING THROUGH 'FOR' (100 to 0)] i = ${i}`);
+}
+
+// Iterating from 0 to 9 and from 100 to 0 (with step equals 10) through 'while' loop.
+
+let i = 0;
+
+while (i <= 9) {
+    console.log(`[ITERATING THROUGH 'WHILE' (0 to 9)] i = ${i}`);
+    i++;
+}
+
+i = 100;
+
+while (i >= 0) {
+    console.log(`[ITERATING THROUGH 'WHILE' (100 to 0)] i = ${i}`);
+    i -= 10;
+}
+
+// Iterating from 0 to 9 and from 100 to 0 (with step equals 10) through 'do...while' loop.
+
+i = 0;
+
+do {
+    console.log(`[ITERATING THROUGH 'DO...WHILE' (0 to 9)] i = ${i}`);
+    i++;
+} while (i <= 9);
+
+i = 100;
+
+do {
+    console.log(`[ITERATING THROUGH 'DO...WHILE' (100 to 0)] i = ${i}`);
+    i -= 10;
+} while (i >= 0);
