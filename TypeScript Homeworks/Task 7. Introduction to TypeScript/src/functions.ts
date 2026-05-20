968fb9acae026979b9330d8c095112d03a76071a
Wollene <whoifnotwollene@gmail.com>
2026-05-20 11:03:38 +0300

    Refactored functions.ts to throw and error in case empty arrays were provided.

--- TypeScript Homeworks/Task 7. Introduction to TypeScript/src/functions.ts
+++ TypeScript Homeworks/Task 7. Introduction to TypeScript/src/functions.ts
@@ -2,25 +2,21 @@ export const numbersArray = [4, 8, 15, 16, 23, 42];
 export const stringsArray = ['Hello', 'Hey', 'Hola'];
 export const mixedArray = ['Hello', 1, 2, 3];
 export const notAnArray = 1;
+const emptyNumberArray: number[] = [];
+const emptyStringArray: string[] = [];
 
 function addEntitiesInArray(array: string[] | number[]): string | number {
-    let sum;
-    if (array.length > 1 && typeof array[0] === 'number') {
-        sum = 0;
-        for (const item of array) {
-            sum = sum + item;
-        }
-        return sum;
-    } else {
-        sum = '';
-        for (const item of array) {
-            sum = sum + item;
-        }
-        return sum;
+    if (array.length === 0) throw new Error('Array is empty!');
+    let sum = typeof array[0] === 'number' ? 0 : '';
+    for (const item of array) {
+        sum += item;
     }
+    return sum;
 }
 
 console.log(`Numbers Array: => ${addEntitiesInArray(numbersArray)}
 Strings Array: => ${addEntitiesInArray(stringsArray)}
 Mixed Array: => addEntitiesInArray(mixedArray) (${mixedArray}) won't compile
-Not an Array: => addEntitiesInArray(notAnArray) (${notAnArray}) won't compile`);
+Not an Array: => addEntitiesInArray(notAnArray) (${notAnArray}) won't compile
+Empty Number Array: => addEntitiesInArray(emptyNumberArray) (${emptyNumberArray}) will throw an error
+Empty String Array: => addEntitiesInArray(emptyStringArray) (${emptyStringArray}) will throw an error`);
