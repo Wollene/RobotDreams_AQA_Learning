c9c22ed2b15850eb90f55ba8b294cdcbc8476675
Wollene <whoifnotwollene@gmail.com>
2026-05-25 21:13:37 +0300

    Reworking inheritance within Task 8, making it more consistent.

--- TypeScript Homeworks/Task 8. Interfaces, Types and Classes/src/classes/User.ts
+++ TypeScript Homeworks/Task 8. Interfaces, Types and Classes/src/classes/User.ts
@@ -1,44 +1,14 @@
+import { IUser } from '../interfaces/user.interfaces';
 import { BaseUser } from './BaseUser';
 
 export class User extends BaseUser {
     private _firstName: string;
     private _lastName: string;
-    private name: string;
-    private username: string;
-    private address: {
-        street: string;
-        suite: string;
-        city: string;
-        zipcode: string;
-        geo: {
-            lat: number;
-            lng: number;
-        }
-    };
-    private company: {
-        name: string;
-        catchPhrase: string;
-        bs: string;
-    };
 
-    public constructor(user: User) {
-        super();
+    public constructor(user: IUser) {
+        super(user);
         this._firstName = user.name.split(' ')[0];
         this._lastName = user.name.split(' ')[1];
-        this.name = user.name;
-        this.username = user.username;
-        this.address = user.address;
-        this.address.street = user.address.street;
-        this.address.suite = user.address.suite;
-        this.address.city = user.address.city;
-        this.address.zipcode = user.address.zipcode;
-        this.address.geo = user.address.geo;
-        this.address.geo.lat = user.address.geo.lat;
-        this.address.geo.lng = user.address.geo.lng;
-        this.company = user.company;
-        this.company.name = user.company.name;
-        this.company.name = user.company.catchPhrase;
-        this.company.bs = user.company.catchPhrase;
     }
 
     public get firstName(): string {
